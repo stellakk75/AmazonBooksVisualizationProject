76add1e1af591e98849c8b5fc1f44e1e7d3d1d5b
pparashar68 <pparashar68@gmail.com>
2021-01-23 15:20:59 -0800

    removed additional table schema and load logic now only one table for the project

--- Database/books_schema.sql
+++ Database/books_schema.sql
@@ -1,23 +1,5 @@
-CREATE TABLE authors (
-author_id	INTEGER primary key,
-author_name VARCHAR(1000)	
-);
-
-CREATE TABLE books(
-book_id INTEGER primary key,
-book_name	VARCHAR(1000),
-author_id INTEGER REFERENCES authors(author_id));
-
-CREATE TABLE books_details(
-book_id	INTEGER REFERENCES books(book_id),
-reviews INTEGER,
-rating	NUMERIC,
-price		NUMERIC,
-year	INTEGER,
-genre			VARCHAR(50)	
-);
 -- Load data into one full table
-CREATE TABLE top_books(
+CREATE TABLE books(
 name	VARCHAR(1000),
 author	VARCHAR(100),
 rating	NUMERIC,
