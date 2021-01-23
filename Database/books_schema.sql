d12283e0b8ff49063b94f3308c97108b4cecade4
pparashar68 <pparashar68@gmail.com>
2021-01-23 14:00:10 -0800

    Added logic to loadn just one table

--- Database/books_schema.sql
+++ Database/books_schema.sql
@@ -16,3 +16,13 @@ price		NUMERIC,
 year	INTEGER,
 genre			VARCHAR(50)	
 );
+-- Load data into one full table
+CREATE TABLE top_books(
+name	VARCHAR(1000),
+author	VARCHAR(100),
+rating	NUMERIC,
+reviews INTEGER,
+price	NUMERIC,
+year	INTEGER,
+genre			VARCHAR(50)	
+);
