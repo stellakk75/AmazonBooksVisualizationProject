3220117dc090cc2cf2a6cca418f8fdcdb193f73d
Sydney Harkins <sydneyharkins95@gmail.com>
2021-01-23 14:12:05 -0500

    Worked on bar graph files

--- scripts/plotlyapp.js
+++ scripts/plotlyapp.js
@@ -22,22 +22,34 @@ readData()
 
 
 // Create bar chart 
-// function buildPlots(value) {
-//     // select dropdown menu
-//     dropdown = d3.select('#selDataset')
-//     // assign value of change id to variable
-//     let id = dropdown.property("value")
+function buildPlots(value) {
+    // select dropdown menu
+    dropdown = d3.select('#selDataset')
+    // assign value of change id to variable
+    let id = dropdown.property("value")
     
-//     d3.csv('../new_books.csv').then(data => {
+    d3.json('/RawData/new_books.json').then(data => {
 
-//         console.log(data)
+        // console.log(data)
         
-//         let level = data.new_books;
-        
-//         let array = level.filter(booksellersObj => booksellersObj.Price == Price);
-//         let result = array[0];
-//         let sample_values = result.sample_values;
-//         let otu_ids = result.otu_ids;
+        let level = data.books;
+        // console.log(level)
+        // let array = filter(bestsellersObj => bestsellersObj.Year == value);
+        // console.log(array)
+        let price = []
+        // for (var i = 0; i < books.Price.length; i++) {
+        //     let price = books[i];
+        // }
+        level.forEach(index => {
+            // console.log(index.Price)
+            if (index.Year == 2009) {
+                price.push(index.Price)
+            }
+            
+            
+        });
+        console.log(price)
+//         let names = books.otu_ids;
 //         let otu_labels = result.otu_labels;
 //         // slice first 10 values for bar chart
 //         let barTrace = {
@@ -46,7 +58,7 @@ readData()
 //             type: "bar",
 //             orientation: 'h',
 //             text: Author.slice(0, 10).reverse()
-//         };
+        // };
 //         // Create the data array for the bar plot
 //         let barData = [barTrace];
 //         Plotly.newPlot("bar", barData);
@@ -80,12 +92,12 @@ readData()
 //     returnKeyValue.forEach(d => {
 //         demographic.append('tr').text(d[0] + ': ' + d[1])
 //     });
-//     });
-// };
+    });
+};
 // function start(){
 //     readData()
 //     d3.select('#selDataset').on('change', buildPlots)
 // }
 // start()
 
-// buildPlots();
+buildPlots("2016");
