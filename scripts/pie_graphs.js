25c50654d670ad65cec1400c758d9b3ea842ee72
Hanna Lee <sarah@Hannas-MacBook-Pro.local>
2021-01-25 11:02:14 -0500

    .

--- scripts/pie_graphs.js
+++ scripts/pie_graphs.js
@@ -1,76 +1,127 @@
 
-// set the dimensions and margins of the graph
-let width = 1750
-    height = 550
-    margin = 40
+function readData(){
+  // select dropdown menu
+  let dropdown = d3.select('#selDataset');
 
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin
+  // read in the booksellers.csv file
+  let filepath = "../RawData/genre.csv"
 
-// append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  d3.csv(filepath).then(data => {
+      data.forEach(function(year) {
+          dropdown.append("option").text(year.Year).property('value');
 
-// create 2 data_set
-d3.csv("../RawData/genre.csv").then(data => {
-    let nonfiction = data.nonfiction;
-    let fiction = data.fiction;
-})
-
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(["a", "b", "c", "d", "e", "f"])
-  .range(d3.schemeDark2);
-
-// A function that create / update the plot for a given variable:
-function update(data) {
-
-  // Compute the position of each group on the pie:
-  var pie = d3.pie()
-    .value(function(d) {return d.value; })
-    .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
-  var data_ready = pie(d3.entries(data))
-
-  // map to data
-  var u = svg.selectAll("path")
-    .data(data_ready)
-
-  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-  u
-    .enter()
-    .append('path')
-    .merge(u)
-    .transition()
-    .duration(1000)
-    .attr('d', d3.arc()
-      .innerRadius(0)
-      .outerRadius(radius)
-    )
-    .attr('fill', function(d){ return(color(d.data.key)) })
-    .attr("stroke", "white")
-    .style("stroke-width", "2px")
-    .style("opacity", 1)
-
-
-  // remove the group that is not present anymore
-  u
-    .exit()
-    .remove()
-
+      })
+  })
 }
 
-// Initialize the plot with the first dataset
-update(data1)
+function optionChanged(val) {
+  // console.log(val)
+  buildPlots(val)
+}
+
+function buildPlots() {
+
+  // select dropdown menu
+  dropdown = d3.select('#selDataset')
+
+  // assign value of change id to variable
+  let id = dropdown.property("value")
+
+  // grabing the genre count from the csv file 
+  d3.csv('../RawData/genre.csv').then(data => {
+   
+    let array = data.filter(row => row.year === id);
+    
+      array.forEach(index => {
+
+       console.log(index);
+      
+          if (array.index.genre == "Fiction") {
+    
+
+            countfinction = array.index.count;
+          } 
+          else {countNonfiction = array.index.count;}
+
+      }); return array;
+
+      let pieTrace = {
+        type: "pie",
+        values: [countfiction, countNonfiction],
+        lables: ["Fiction", "Non Fiction"],
+        textinfo: "lable+percent"
+      }
+
+      let pielayout = [{
+        title: `Non fiction vs Fiction in ${val}`
+      }];
+
+      Plotly.newPlot("pie", pieTrace, pielayout);
+    }); 
+  };
 
 
-  
 
 
+// // set the dimensions and margins of the graph
+// var width = 450
+//     height = 450
+//     margin = 40
 
+// // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+// var radius = Math.min(width, height) / 2 - margin
 
+// // append the svg object to the div called 'my_dataviz'
+// var svg = d3.select("#my_dataviz")
+//   .append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//   .append("g")
+//     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// // create 2 data_set
+// var data1 = {a: 9, b: 20, c:30, d:8, e:12}
+// var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
+
+// // set the color scale
+// var color = d3.scaleOrdinal()
+//   .domain(["a", "b", "c", "d", "e", "f"])
+//   .range(d3.schemeDark2);
+
+// // A function that create / update the plot for a given variable:
+// function update(data) {
+
+//   // Compute the position of each group on the pie:
+//   var pie = d3.pie()
+//     .value(function(d) {return d.value; })
+//     .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
+//   var data_ready = pie(d3.entries(data))
+
+//   // map to data
+//   var u = svg.selectAll("path")
+//     .data(data_ready)
+
+//   // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+//   u
+//     .enter()
+//     .append('path')
+//     .merge(u)
+//     .transition()
+//     .duration(1000)
+//     .attr('d', d3.arc()
+//       .innerRadius(0)
+//       .outerRadius(radius)
+//     )
+//     .attr('fill', function(d){ return(color(d.data.key)) })
+//     .attr("stroke", "white")
+//     .style("stroke-width", "2px")
+//     .style("opacity", 1)
+
+//   // remove the group that is not present anymore
+//   u
+//     .exit()
+//     .remove()
+
+// }
 
 
--- scripts/plotlyapp.js
+++ scripts/plotlyapp.js
@@ -3,22 +3,15 @@ function readData(){
     let dropdown = d3.select('#selDataset');
 
     // read in the booksellers.csv file
-    // let filepath = "../RawData/bestsellers.csv"
+    let filepath = "../RawData/bestsellers.csv"
 
-<<<<<<< HEAD
-    // d3.csv(filepath).then(data => {
+    d3.csv(filepath).then(data => {
 
-    //     data.forEach(function(year) {
-    //         dropdown.append("option").text(year.Year).property('value');
-    //     })
-    // })
+        data.forEach(function(year) {
+            dropdown.append("option").text(year.Year).property('value');
+        })
+    })
 }
-=======
-        // years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-        // years.forEach(function(year) {
-        //     dropdown.append("option").text(year).property('value');
-        }
->>>>>>> 1301241721791d01197d80e0082bff8e9bdec2fa
 
 
 function optionChanged(val) {
@@ -32,51 +25,31 @@ function optionChanged(val) {
   }
 
 
-<<<<<<< HEAD
   function buildPlots(val) {
-=======
-// Create bar chart 
-function buildPlots(val) {
->>>>>>> 1301241721791d01197d80e0082bff8e9bdec2fa
+
     // select dropdown menu
     dropdown = d3.select('#selDataset')
+    
     // assign value of change id to variable
     let id = dropdown.property("value")
-<<<<<<< HEAD
     d3.csv('../RawData/filtered-bar-plot-data.csv').then(data => {
-=======
-    
-    d3.csv('../RawData/filtered-bar-plot-data.csv').then(data => {
-        
->>>>>>> 1301241721791d01197d80e0082bff8e9bdec2fa
         // let level = data.books;
         // console.log(array)
-        let bookRating = []
+        
             // console.log(index.Rating)
             // if (index.Rating >= 0 && index.Rating <= 5.0) {
             //     rating.push(index.Rating)
             let array = data.filter(row => row.year === id);
-<<<<<<< HEAD
+            
         array.forEach(index => {
+
+            let rating = []
+            
             rating.push(array[index].rating)
         });
         // console.log(rating)
         // let bookRating = array[1].rating;
         // let bookName = array[1].name;
-=======
-
-        array.forEach(index => {
-            // rating.push(array[index].rating)
-            if (index.Rating >= 0 && index.Rating <= 5.0) {
-                rating.push(index.Rating)
-        }
-        console.log(rating)
-    }
-        // let bookRating = array[1].rating;
-        let bookName = array[1].name;
-
-        
->>>>>>> 1301241721791d01197d80e0082bff8e9bdec2fa
         // // slice first 10 values for bar chart
         // let barTrace = {
         //     x: bookRating.slice(0, 10).reverse(),
@@ -84,7 +57,6 @@ function buildPlots(val) {
         //     type: "bar",
         //     orientation: 'h'
         // };
-<<<<<<< HEAD
         // // Create the data array for the bar plot
         // let barData = [barTrace];
         // Plotly.newPlot("bar", barData);
@@ -95,19 +67,4 @@ function start() {
     readData()
     d3.select('#selDataset').on('change', buildPlots)
 }
-=======
-
-        // // Create the data array for the bar plot
-        // let barData = [barTrace];
-
-        // Plotly.newPlot("bar", barData);
-    });
-
-function start() {
-    readData()
-
-    d3.select('#selDataset').on('change', buildPlots)
-}
-
->>>>>>> 1301241721791d01197d80e0082bff8e9bdec2fa
 start()
