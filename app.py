d146789e8763151e497c45b695dbe6b88544b8fe
stellakk75 <70276685+stellakk75@users.noreply.github.com>
2021-01-25 15:54:02 -0500

    authors

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def home():
 def authors():
     authors_df = pd.read_sql_query(
                    ''' SELECT DISTINCT author FROM books \
-                       ORDER BY author DESC
+                       ORDER BY author ASC
                    ''' , db)
     i = 0
     authors_list = []
@@ -47,49 +47,77 @@ def authors():
         authors = authors_df.to_dict('split')['data'][i][0]
 
         data = {
-            'Author Name' : authors
+            'Author' : authors
         }
         authors_list.append(data)
     return jsonify(authors_list)
 
-@app.route("/avgprice_yearly")
-def avgprice_yearly():
-    avgprice_df = pd.read_sql_query(
-                   '''  SELECT year,ROUND(avg(price),2) FROM books \
-                        GROUP BY year
-                        ORDER BY year;
+@app.route("/author_books")
+def author_books():
+    authors_books_df = pd.read_sql_query(
+                   ''' SELECT * FROM books \
+                       ORDER BY author asc
                    ''' , db)
-    i = 0
-    avgprice_list = []
-    for i in range(len(avgprice_df.to_dict('split')['data'])):
-        year = avgprice_df.to_dict('split')['data'][i][0]
-        avgprice = avgprice_df.to_dict('split')['data'][i][1]
+    author_books_list = [ ]
+    for i in range(len(authors_books_df.to_dict('split')['data'])):
+        name = authors_books_df.to_dict('split')['data'][i][0]
+        author = authors_books_df.to_dict('split')['data'][i][1]
+        rating = authors_books_df.to_dict('split')['data'][i][2]
+        review = authors_books_df.to_dict('split')['data'][i][3]
+        price = authors_books_df.to_dict('split')['data'][i][4]
+        year = authors_books_df.to_dict('split')['data'][i][5]
+        genre = authors_books_df.to_dict('split')['data'][i][6]
+   
+        data = { 
+                "Author": author,
+                "Title": name,
+                "Reviews": review,
+                "Rating": rating,
+                "Price": price,
+                "Year": year,
+                "Genre": genre
+                }
+        author_books_list.append(data)
+    return jsonify(author_books_list)
 
-        data = {
-            'Year' : year,
-            'Average Price' : avgprice
-        }
-        avgprice_list.append(data)
-    return jsonify(avgprice_list)
+# @app.route("/avgprice_yearly")
+# def avgprice_yearly():
+#     avgprice_df = pd.read_sql_query(
+#                    '''  SELECT year,ROUND(avg(price),2) FROM books \
+#                         GROUP BY year
+#                         ORDER BY year;
+#                    ''' , db)
+#     i = 0
+#     avgprice_list = []
+#     for i in range(len(avgprice_df.to_dict('split')['data'])):
+#         year = avgprice_df.to_dict('split')['data'][i][0]
+#         avgprice = avgprice_df.to_dict('split')['data'][i][1]
 
-@app.route("/avg_rating")
-def avg_rating():
-    avg_rating_df = pd.read_sql_query(
-                   ''' SELECT year,ROUND(avg(rating),2) FROM books \
-                       GROUP BY year \
-                       ORDER BY year
-                   ''' , db)
-    i = 0
-    avg_rating_list = []
-    for i in range(len(avg_rating_df.to_dict('split')['data'])):
-        year = avg_rating_df.to_dict('split')['data'][i][0]
-        rating =  avg_rating_df.to_dict('split')['data'][i][1]
-        data = {
-            'Year': year,
-            'Average Rating': rating
-        }
-        avg_rating_list.append(data)
-    return jsonify(avg_rating_list)
+#         data = {
+#             'Year' : year,
+#             'Average Price' : avgprice
+#         }
+#         avgprice_list.append(data)
+#     return jsonify(avgprice_list)
+
+# @app.route("/avg_rating")
+# def avg_rating():
+#     avg_rating_df = pd.read_sql_query(
+#                    ''' SELECT year,ROUND(avg(rating),2) FROM books \
+#                        GROUP BY year \
+#                        ORDER BY year
+#                    ''' , db)
+#     i = 0
+#     avg_rating_list = []
+#     for i in range(len(avg_rating_df.to_dict('split')['data'])):
+#         year = avg_rating_df.to_dict('split')['data'][i][0]
+#         rating =  avg_rating_df.to_dict('split')['data'][i][1]
+#         data = {
+#             'Year': year,
+#             'Average Rating': rating
+#         }
+#         avg_rating_list.append(data)
+#     return jsonify(avg_rating_list)
 
 @app.route("/avg_rating_by_author")
 def avg_rating_by_author():
@@ -104,8 +132,8 @@ def avg_rating_by_author():
         author = avg_author_rating_df.to_dict('split')['data'][i][0]
         rating =  avg_author_rating_df.to_dict('split')['data'][i][1]
         data = {
-            'Author Name': author,
-            'Average Rating': rating
+            'Author': author,
+            'AverageRating': rating
         }
         avg_author_rating_list.append(data)
     return jsonify(avg_author_rating_list)
--- static/scripts/author.js
+++ static/scripts/author.js
@@ -1,177 +1,108 @@
-function readData(){
     // select dropdown menu
     let dropdown = d3.select('#selDataset');
 
     // read in the booksellers.csv file
     // replace with Pankaj's sql qry authors
-    let filepath = "../RawData/new_books.json"
-
+    let filepath = "http://127.0.0.1:5000/authors"
+ 
     d3.json(filepath).then(data => {
         // console.log(data)
-
-        let level = data.books
-        level.forEach(author => {
-            dropdown.append("option").text(author.Author).property('value');
-        })
-  
-        let test_result = level[0]
-        console.log(test_result)
-  
-        let book_info = d3.select('#book-table')
-        keyValue = Object.entries(test_result)
-        keyValue.forEach(d => {
-            book_info.append('tr').text(d[0]+': '+d[1])
+        data.forEach(item => {
+            dropdown.append("option").text(item.Author).property('value');
         })
 
-        var bonus = [
-                {
-                    domain: { x: [0,1], y: [0, 1] },
-                    type: 'indicator',
-                    value: test_result.Rating,
-                    title: { text: "<b>Rating</b>" },
-                    gauge: { axis: { range: [null,5] },
-                        steps: [{range:[0,1]},{range:[1,2]},{range:[2,3]},{range:[3,4]},
-                        {range:[4,5]}]},
-                    mode: "gauge+number",
-                }];
-            Plotly.newPlot('gauge', bonus);
-        })
+        //select dropdown menu 
+        d3.select('#selDataset').on('change', buildBar)
 
+        // assigns value of change id to variable 
 
-        // let trace = {
-        //     y: otu_ids.slice(0,10).map(item => "OTU " + item).reverse(),
-        //     x: sample_values.slice(0,10).reverse(),
-        //     orientation: 'h',
-        //     text: otu_labels.slice(0,10).reverse(),
-        //     type: "bar"
-        //   };
-        // let barchart = [trace];
-        // Plotly.newPlot("bar", barchart);
-        // let trace_bubble = {
-        //     y: result.sample_values,
-        //     x: result.otu_ids,
-        //     mode: 'markers',
-        //     marker: {
-        //         size: sample_values,
-        //         color: result.otu_ids,
-        //     },
-        //     text: result.otu_labels,
-        //     type: "bubble"
-        //   };
-        // let bubble_chart = [trace_bubble];
-        // let bubble_layout =  {
-        //     xaxis: {title:'OTU ID'}
-        // }
-        // Plotly.newPlot("bubble", bubble_chart, bubble_layout);
-        // let array2 = data.metadata.filter(sampleObj => sampleObj.id == 940);
-        // let result2 = array2[0]   
-        // let demographic = d3.select('#sample-metadata')
-        // keyValue = Object.entries(result2)
-        // keyValue.forEach(d => {
-        //     demographic.append('tr').text(d[0]+': '+d[1])
-        // })
-
-//Attempt of bonus 
+    })
     
+function buildBar(name){
+    //select dropdown menu 
+    dropDown = d3.select('#selDataset')
+    // assigns value of change id to variable 
+    let id = dropDown.property("value")
+    // console.log(id)
 
-}
+    
+    
+    var svg = d3.select("body").append("svg")
+	.attr("height", 960)
+	.attr("width", 1000);
 
-readData()
+    var table = d3.select("#table-location")
+        .append("table")
+        .attr("class", "table table-condensed table-striped"),
+        thead = table.append("thead"),
+        tbody = table.append("tbody");
 
-// //main function 
-// function buildBar(value){
-//     //select dropdown menu 
-//     dropDown = d3.select('#selDataset')
-//     // assigns value of change id to variable 
-//     let id = dropDown.property("value")
-
-//     //read file. then grab relevant data and create visualizations accordingly 
-//     d3.json('samples.json').then(data=>{
-
-//         //filter for user ID = JSON file ID 
-//         let level = data.samples;
-//         let array = level.filter(sampleObj => sampleObj.id == id);
-//         let result = array[0];
-
-//         // pull out JSON info for charts 
-//         let sample_values = result.sample_values;
-//         let otu_ids = result.otu_ids
-//         let otu_labels = result.otu_labels
-
-// // ------------------------------------------//
-//         //slice first 10 values for horizontal bar chart 
-//         let trace = {
-//             y: otu_ids.slice(0,10).map(item => "OTU " + item).reverse(),
-//             x: sample_values.slice(0,10).reverse(),
-//             orientation: 'h',
-//             text: otu_labels.slice(0,10).reverse(),
-//             type: "bar"
-//           };
-
-//         let barchart = [trace];
-
-//         Plotly.newPlot("bar", barchart);
-// // ------------------------------------------//
-//         // create bubble chart 
-//         let trace_bubble = {
-//             y: result.sample_values,
-//             x: result.otu_ids,
-//             mode: 'markers',
-//             marker: {
-//                 size: sample_values,
-//                 color: result.otu_ids,
-//             },
-//             text: result.otu_labels,
-//             type: "bubble"
-//           };
+    //read file. then grab relevant data and create visualizations accordingly 
+    d3.json('http://127.0.0.1:5000/author_books').then(item=>{
+        // console.log(item)
         
-//         let bubble_chart = [trace_bubble];
+        let data = item.filter(row => row.Author === id);
+        // console.log(data)
 
-//         let bubble_layout =  {
-//             xaxis: {title:'OTU ID'}
-//         }
-          
-//         Plotly.newPlot("bubble", bubble_chart, bubble_layout);
-// // --------------------------------------------------------------//
-//     // Display the sample's metadata (demographic information)
-//     let level2 = data.metadata
-//     let array2 = level2.filter(sampleObj => sampleObj.id == id);
-//     let result2 = array2[0]
+        var columns = Object.keys(data[0])
+		.filter(function(d){
+			return ((d != "Author" ));
+        });
+        // console.log(columns)
+        
+        var header = thead.append("tr")
+		.selectAll("th")
+		.data(columns)
+		.enter()
+		.append("th")
+			.text(function(d){ return d;})
+              
+        var rows = tbody.selectAll("tr")
+        .data(data)
+        .enter()
+        .append("tr")
+        .on("mouseover", function(d){
+            d3.select(this)
+                .style("background-color", "orange");
+        })
+        .on("mouseout", function(d){
+            d3.select(this)
+                .style("background-color","transparent");
+        });
 
-//     let demographic = d3.select('#sample-metadata')
+    	var cells = rows.selectAll("td")
+		.data(function(row){
+			return columns.map(function(d, i){
+				return {i: d, value: row[d]};
+			});
+		})
+		.enter()
+		.append("td")
+		.html(function(d){ return d.value;});		
+    })
+
+//rating gauge chart 
+let filepath = "http://127.0.0.1:5000/avg_rating_by_author"
+d3.json(filepath).then(index => {
+    // console.log(index)
+
+    let new_data = index.filter(row => row.Author === id);
+    // console.log(new_data.length)
+    // console.log(new_data)
+    // console.log(new_data[0].AverageRating)
+
+    let bonus = [
+        {
+            domain: { x: [0,1], y: [0, 1] },
+            type: 'indicator',
+            value: new_data[0].AverageRating,
+            title: { text: "<b>Rating</b>" },
+            gauge: { axis: { range: [null,5] },
+                steps: [{range:[0,1]},{range:[1,2]},{range:[2,3]},{range:[3,4]},
+                {range:[4,5]}]},
+            mode: "gauge+number",
+        }];
+    Plotly.newPlot('gauge', bonus);   
+})
     
-//     //clear data 
-//     demographic.html("");
-//     //returns key and value of result2 array
-//     keyValue = Object.entries(result2)
-//     // console.log(keyValue)
-//     // for each keyvalue in result2 array, append to demographic info
-//     keyValue.forEach(d => {
-//         demographic.append('tr').text(d[0]+': '+d[1])
-//     })
-// // --------------------------------------------------------------//
-// //bonus: Gauge Chart
-//     var bonus = [
-//         {
-//             domain: { x: [0,1], y: [0, 1] },
-//             type: 'indicator',
-//             value: result2.wfreq,
-//             title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" },
-//             gauge: { axis: { range: [null,9] },
-//                 steps: [{range:[0,1]},{range:[1,2]},{range:[2,3]},{range:[3,4]},
-//                 {range:[4,5]},{range:[5,6]},{range:[6,7]},{range:[7,8]},{range:[8,9]}]},
-//             mode: "gauge+number",
-//         }]
-
-//     Plotly.newPlot('gauge', bonus);
-//     })
-// };
-
-// //starter function 
-// function start(){
-//     populate()
-//     // if dropdown menu changes, then buildBar 
-//     d3.select('#selDataset').on('change', buildBar)
-// }
-
-// start()
+}
