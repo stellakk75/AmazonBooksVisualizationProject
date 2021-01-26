18626560c14b4ca03666e33db7d7822e24a20332
Sydney Harkins <sydneyharkins95@gmail.com>
2021-01-25 19:34:41 -0500

    Added working bar plot code

--- scripts/plotlyapp.js
+++ scripts/plotlyapp.js
@@ -1,69 +1,112 @@
 function readData(){
     // select dropdown menu
     let dropdown = d3.select('#selDataset');
-
     // read in the booksellers.csv file
-    let filepath = "../RawData/bestsellers.csv"
-
-        // years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-        // years.forEach(function(year) {
-        //     dropdown.append("option").text(year).property('value');
-        }
-
-
-readData()
-
+    // let filepath = "../RawData/bestsellers.csv"
+    // d3.csv(filepath).then(data => {
+    //     data.forEach(function(year) {
+    //         dropdown.append("option").text(year.Year).property('value');
+    //     })
+    // })
+}
 function optionChanged(val) {
     console.log(val)
-    buildPlots(val)
-  }
+  };
 
-
-// Create bar chart 
-function buildPlots(val) {
+  function buildPlots(val) {
     // select dropdown menu
     dropdown = d3.select('#selDataset')
     // assign value of change id to variable
     let id = dropdown.property("value")
-    
     d3.csv('../RawData/filtered-bar-plot-data.csv').then(data => {
-        
-        // let level = data.books;
-        // console.log(array)
-        let bookRating = []
+        let ratingArray = []
+        let nameArray = []
             // console.log(index.Rating)
             // if (index.Rating >= 0 && index.Rating <= 5.0) {
             //     rating.push(index.Rating)
             let array = data.filter(row => row.year === id);
 
         array.forEach(index => {
-            // rating.push(array[index].rating)
-            if (index.Rating >= 0 && index.Rating <= 5.0) {
-                rating.push(index.Rating)
-        }
-        console.log(rating)
-    }
-        // let bookRating = array[1].rating;
-        let bookName = array[1].name;
+            ratingArray.push(index.rating)
+        });
+        // console.log(ratingArray)
 
+        array.forEach(index => {
+            nameArray.push(index.name)
+        });
+        // console.log(nameArray)
+
+        array.forEach(index => {
+            if (data.price > 20) {
+                return above20Trace
+            }
+        });
+
+        array.forEach(index => {
+            if (data.price > 10 && data.price < 20) {
+                return above10Trace
+            }
+        });
+
+        array.forEach(index => {
+            if (data.price < 10) {
+                return below10Trace
+            }
+        });
+
+        let bookRating = ratingArray
+        let bookName = nameArray
+
+        // slice first 10 values for bar chart
+        let above20Trace = {
+            x: bookRating.slice(0, 10).reverse(),
+            y: bookName.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: 'h',
+            marker: {
+                color: 'rgb(191, 63, 63)'
+            }
+        };
         
-        // // slice first 10 values for bar chart
-        // let barTrace = {
-        //     x: bookRating.slice(0, 10).reverse(),
-        //     y: bookName.slice(0, 10).reverse(),
-        //     type: "bar",
-        //     orientation: 'h'
-        // };
+        let above10Trace = {
+            x: bookRating.slice(0, 10).reverse(),
+            y: bookName.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: 'h',
+            marker: {
+                color: 'rgb(191, 63, 127)'
+            }
+        };
 
-        // // Create the data array for the bar plot
-        // let barData = [barTrace];
+        let below10Trace = {
+            x: bookRating.slice(0, 10).reverse(),
+            y: bookName.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: 'h',
+            marker: {
+                color: 'rgb(63, 63, 191)'
+            }
+        };
 
-        // Plotly.newPlot("bar", barData);
-    });
+        //create the data array for the bar plot
+        let barData = [above20Trace, above10Trace, below10Trace];
+
+        // create the plot layout
+        let barLayout = {
+            title: 'Top 10 Amazon Bestsellers',
+            font: {
+                family: "Raleway",
+            },
+            showlegend: true
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+})
+
+}
 
 function start() {
     readData()
-
     d3.select('#selDataset').on('change', buildPlots)
 }
 
